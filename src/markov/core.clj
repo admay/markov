15c1cd8f651e2a4abb349442a5ed08ba902b038f
Michael <zavarella.dev@gmail.com>
2017-01-24 01:00:07 +0000

    Added functionality to convert text of a word chain map

--- src/markov/core.clj
+++ src/markov/core.clj
@@ -1,4 +1,25 @@
-(ns markov.core)
+(ns markov.core
+  (:gen-class))
+
+(def example "And the Golden Grouse And the Pobble who")
+
+(defn word-chain [word-transitions]
+  (reduce (fn [r [p1 p2 s]] (merge-with clojure.set/union r
+                                        {[p1 p2] (if s #{s} #{})}))
+          {} word-transitions))
+
+(defn text->word-chain
+  [text]
+  (let [words (clojure.string/split text #"[\s|\n]")
+        word-transitions (partition-all 3 1 words)]
+    (word-chain word-transitions)))
+
+(text->word-chain example)
+
+
+
+
+
 
 (defn -main
   "I don't do a whole lot ... yet."
