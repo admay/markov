a61d5b8a311e5ea63e7e529b737c2976af843347
Michael <zavarella.dev@gmail.com>
2017-01-24 02:17:25 +0000

    Added requires' to the ns declaration. Added generate text function.

--- src/markov/core.clj
+++ src/markov/core.clj
@@ -1,27 +1,53 @@
 (ns markov.core
-  (:gen-class))
-
-(def example "And the Golden Grouse And the Pobble who")
+  (:require [clojure.string :as s]
+            [clojure.set :as set]))
 
 (defn word-chain [word-transitions]
-  (reduce (fn [r [p1 p2 s]] (merge-with clojure.set/union r
+  (reduce (fn [r [p1 p2 s]] (merge-with set/union r
                                         {[p1 p2] (if s #{s} #{})}))
           {} word-transitions))
 
 (defn text->word-chain
   [text]
-  (let [words (clojure.string/split text #"[\s|\n]")
+  (let [words (s/split text #"[\s|\n]")
         word-transitions (partition-all 3 1 words)]
     (word-chain word-transitions)))
 
+(defn chain->text
+  [chain]
+  (apply str (interpose " " chain)))
+
 (defn walk-chain
-  [prefix chain result]
+  [prefix chain result char-limit]
   (let [suffixes (get chain prefix)]
     (if (empty? suffixes)
       result
       (let [suffix (first (shuffle suffixes))
-            new-prefix [(last prefix) suffix]]
-        (recur new-prefix chain (conj result suffix))))))
+            new-prefix [(last prefix) suffix]
+            result-with-spaces (chain->text result)
+            result-char-count (count result-with-spaces)
+            suffix-char-count (inc (count suffix))
+            new-result-char-count (+ result-char-count suffix-char-count)]
+        (if (>= new-result-char-count char-limit)
+          result
+          (recur new-prefix chain (conj result suffix) char-limit))))))
+
+(defn generate-text
+  [start-phrase word-chain char-limit]
+  (let [prefix (s/split start-phrase #" ")
+        result-chain (walk-chain prefix word-chain prefix char-limit)
+        result-text (chain->text result-chain)]
+    result-text))
+
+
+
+
+
+
+
+
+
+
 
 
 
